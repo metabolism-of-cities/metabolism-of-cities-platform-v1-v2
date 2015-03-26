812438360d6ba06deab8f9df3e6445aa8e987784
Paul Hoekman <paul@ibisservicios.com>
2015-03-26 09:11:49 +0200

    List slowly growing 2

--- research.list.php
+++ research.list.php
@@ -19,13 +19,13 @@ $list = $db->query("SELECT * FROM research ORDER BY title");
 
 <h1>Current Research - Project List</h1>
 
-<p>In this section we aim to provide a collection of ongoing MFA-related research. We are starting out so there is
-not much to see yet, but we encourage everyone who works on MFA studies to submit their project here! Projects are 
-listed to improve global knowledge of what is going on in the world of MFA studies, and to facilitate communication 
-between researchers. 
+<p>
+In this section we aim to provide a collection of ongoing MFA-related
+research. Our website is still relatively young so the list is slowly growing,
+but we encourage everyone who works on MFA studies to submit their project
+here! Projects are listed to improve global knowledge of what is going on in
+the world of MFA studies, and to facilitate communication between researchers. 
 </p>
-
-<?php if (count($list)) { ?>
 
   <table class="table table-striped">
     <tr>
@@ -42,15 +42,7 @@ between researchers.
   <?php } ?>
   </table>
 
-<?php } else { ?>
-
-  <div class="alert alert-info">
-    We do not have any public dataset online yet! You can be the first by uploading your dataset from a previous project, 
-    or by <a href="projects/add">creating a new project</a>. Alternatively, head to the <a href="publications/list">database</a>
-    to go through Material Flow Analysis publications. 
-  </div>
-
-<?php } ?>
+<p><a href="research/add" class="btn btn-primary">Add your own project</a></p>
 
 <?php require_once 'include.footer.php'; ?>
 
