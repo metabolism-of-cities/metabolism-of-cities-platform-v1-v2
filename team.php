c7bc1d52fcc401edc59a33e105e5d4675c8dd150
Paul Hoekman <paul@ibisservicios.com>
2016-01-22 05:53:52 +0200

    Expanding team 3

--- team.php
+++ team.php
@@ -19,18 +19,31 @@ $page = 3;
         <p>Our team is slowly expanding and we welcome new people! When will you join in?!</p>
         <ul>
           <li>
-          Paul Hoekman - Recently graduated MPhil Student at the University of
+          <strong>Paul Hoekman </strong> - 
+          Recently graduated MPhil Student at the University of
           Cape Town, South Africa - Initial creator of this website while doing
           research into the feasibility of undertaking an urban-scale Material
           Flow Analysis in the South African context.
           </li>
           <li>
-            Aristide Athanassiadis - Joint-PhD student at the Université Libre
+            <strong>Aristide Athanassiadis</strong> - 
+            Joint-PhD student at the Université Libre
             de Bruxelles (Brussels, Belgium) and University of Melbourne,
             Australia. Through different research projects in Brussels and
             Melbourne, he has been interested in identifying the
             context-specific factors that influence urban metabolisms using a
             temporal and spatial perspective. 
+          </li>
+          <li>
+            <strong>Gabriela Fernandez</strong> - 
+            Ph.D. candidate in Urban Planning, Design
+            and Policy in the Department of Architecture and Urban Studies
+            at Politecnico di Milano in Milan, Italy. Fernandez is
+            interested in urban metabolism ideologies and is undertaking a
+            material flow analysis of the Metropolitan City of Milan thus
+            identifying urban typologies and socioeconomic indicators in
+            the Italian urban context while promoting urban metabolism
+            public policy. <a href="https://it.linkedin.com/in/gabriela-fernandez-56873843"><i class="fa fa-linkedin-square"></i></a>
           </li>
         </ul>
         <p>
