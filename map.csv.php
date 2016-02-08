72a7dbd1f9aa78e27a285b1f610c5f8a3c4a044d
Paul Hoekman <paul@penguinprotocols.com>
2016-02-08 09:32:42 +0200

    Improve the map 30

--- map.csv.php
+++ map.csv.php
@@ -1,6 +1,5 @@
 <?php
 require_once 'functions.php';
-
 header("Content-type: text/csv");
 header("Content-Disposition: attachment; filename=publications.csv");
 header("Pragma: no-cache");
@@ -21,21 +20,63 @@ function outputCSV($data) {
 }
 
 $list = $db->query("SELECT 
-  tags.tag, papers.title, papers.author, papers.id
+  tags.tag, papers.title, papers.author, papers.id, tags.id AS tag_id, papers.year
 FROM tags_papers 
   LEFT JOIN tags ON tags_papers.tag = tags.id
   LEFT JOIN papers ON tags_papers.paper = papers.id
 WHERE tags.parent = 4 AND papers.status = 'active'");
 
-$csv[] = array("Publication", "City", "Author(s)", "URL");
+$csv[] = array("Publication(s)", "City", "Author(s)", "URL", "Year", "Quantity");
 
 foreach ($list as $row) {
-  $csv[] = array(
-    $row['title'],
-    strtr($row['tag'], $remove), 
-    $row['author'], 
-    URL . "publication/" . $row['id']
+  // First we do a first loop to group all the same city studies
+  // in one array, so we can see if a city has 1 study or > 1 studies
+  $city = strtr($row['tag'], $remove);
+  $cities[$city][] = array(
+    'title' => html_entity_decode($row['title'], ENT_QUOTES), 
+    'author' => $row['author'], 
+    'link' => URL . "publication/" . $row['id'], 
+    'tag' => $row['tag_id'],
+    'year' => $row['year'],
   );
+}
+
+foreach ($cities as $city => $studies) {
+
+  if (count($studies) > 1) {
+    // There are multiple studies, so we will have to group them onto one line. This is 
+    // done because CartoDB does not (easily) support multiple dots in one place. 
+    // See: https://gis.stackexchange.com/questions/91983
+    $csv[$city][0] = "<ul>";
+    $csv[$city][1] = $city;
+
+    // The author field will remain empty
+    $csv[$city][2] = false;
+
+    foreach ($studies as $row) {
+
+      // Instead of featuring one article title, the title field will contain a list with all publication titles
+      $csv[$city][0] .= "<li><a href='{$row['link']}'>{$row['title']}</a> ({$row['year']})</li>";
+
+      // The link will point to the overview with all studies from this city
+      $csv[$city][3] = URL . "tags/{$row['tag']}/" . flatten($city);
+    }
+
+    $csv[$city][4] = false; // Don't set a year as a separate field
+    $csv[$city][5] = count($studies);
+    $csv[$city][0] .= "</ul>";
+
+  } else {
+    $csv[$city] = array(
+      $studies[0]['title'],
+      $city,
+      $studies[0]['author'],
+      $studies[0]['link'],
+      $studies[0]['year'],
+      1, // The quantity of studies for this city
+    );
+  }
+
 }
 
 outputCSV($csv);
