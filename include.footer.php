dc3ce79f6491399b5db2df81a09142dd285633ce
Paul Hoekman <paul@penguinprotocols.com>
2016-03-29 16:32:12 +0200

    Add github logo in footer 2

--- include.footer.php
+++ include.footer.php
@@ -8,6 +8,7 @@
         <p class="text-muted">
           <a href="page/contact"><i class="fa fa-envelope"></i></a>
           <a href="https://twitter.com/CityMetabolism"><i class="fa fa-twitter"></i></a>
+          <a href="https://github.com/paulhoekman/mfa-tools"><i class="fa fa-github-square"></i></a>
           <a href="https://creativecommons.org/licenses/by/4.0/" class="pull-left">
           Creative Commons Attribution 4.0 International license.
           </a>
