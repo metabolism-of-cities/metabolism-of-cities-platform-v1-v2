34360b58887d6e3ce2c675f80f4709252d77c6ae
Paul Hoekman <paul@penguinprotocols.com>
2016-06-17 09:51:45 -0500

    Full month 1

--- include.footer.php
+++ include.footer.php
@@ -13,7 +13,7 @@
           Creative Commons Attribution 4.0 International license.
           </a>
           <span class="right">
-          Last update: <strong><?php echo format_date("M Y", $update->date_added) ?></strong>
+          Last update: <strong><?php echo format_date("F Y", $update->date_added) ?></strong>
           </span>
         </p>
       </div>
