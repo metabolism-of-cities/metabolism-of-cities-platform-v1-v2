af16af990669dedf19711c5251bb25fd40bcd62c
Paul Hoekman <paul@penguinprotocols.com>
2016-10-18 11:31:15 +0200

    Tweaking the autotweet 10

--- apis/tweet.php
+++ apis/tweet.php
@@ -10,17 +10,29 @@ function getConnectionWithAccessToken($oauth_token, $oauth_token_secret) {
 }
  
 $connection = getConnectionWithAccessToken($access_token, $access_token_secret);
+$file = "../media/dataviz/".$info->id.".jpg";
+$image = $connection->upload('media/upload', ['media' => $file]);
 
 $info = $db->record("SELECT * FROM datavisualizations WHERE date <= CURDATE() ORDER BY date DESC LIMIT 1");
+
+if (!$info->id) {
+    mailadmins("AutoAlert: No automated tweet sent out because no dataviz was found for today. Let's make sure there is one for tomorrow!\nCheers,\nAutoBot");
+    die();
+}
+
 $tweet = "Today's data visualization: " . $info->title;
 
-if (strlen($tweet) > 110) {
-  $tweet = substr($tweet, 0, 107) . "...";
+if (strlen($tweet) > 85) {
+  $tweet = substr($tweet, 0, 82) . "...";
 }
 
 $tweet .= " - " . URL . "datavisualizations/" . $info->id . "-" . flatten($info->title);
 
-$status = $connection->post("statuses/update", ["status" => $tweet]);
+$parameters = [
+    'status' => $tweet,
+    'media_ids' => implode(',', [$image->media_id_string])
+];
+$status = $connection->post("statuses/update", [$parameters]);
 
 if ($connection->getLastHttpCode() == 200) {
     // Tweet posted succesfully
